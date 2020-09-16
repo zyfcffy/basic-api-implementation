6a5bcea35f570527b62ed19e1cdf2fbbf3919b54
FuFangyuan <fangyuan.fu@thoughtworks.com>
2020-09-16 17:42:16 +0800

    User user = new User(Mary, female, null, a@twu.com, 15525557689);

--- src/main/java/com/thoughtworks/rslist/dto/User.java
+++ src/main/java/com/thoughtworks/rslist/dto/User.java
@@ -3,6 +3,7 @@ package com.thoughtworks.rslist.dto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Max;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -24,6 +25,7 @@ public class User {
     private String name;
     private String gender;
     @NotNull
+    @Max(100)
     private Integer age;
     private String email;
     private String phone;
