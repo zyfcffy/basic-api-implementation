ede25a01e1c253417356649b0f025477d108d5aa
FuFangyuan <fangyuan.fu@thoughtworks.com>
2020-09-17 08:41:48 +0800

    [fufangyuan]add logger in exceptionhandler

--- src/main/java/com/thoughtworks/rslist/exceptions/ExceptionHandler.java
+++ src/main/java/com/thoughtworks/rslist/exceptions/ExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.thoughtworks.rslist.exceptions;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 
 @ControllerAdvice
 public class ExceptionHandler {
+    public static Logger logger = LoggerFactory.getLogger(ExceptionHandler.class);
 
     @org.springframework.web.bind.annotation.ExceptionHandler({IndexOutOfBoundsException.class,
             InvalidIndexException.class})
@@ -12,9 +15,11 @@ public class ExceptionHandler {
         CommentError commentError = new CommentError();
         if (ex instanceof IndexOutOfBoundsException) {
             commentError.setError("invalid request param");
+            logger.error("invalid request param");
         }
         if (ex instanceof InvalidIndexException) {
             commentError.setError("invalid index");
+            logger.error("invalid index");
         }
         return ResponseEntity.status(400).body(commentError);
     }
